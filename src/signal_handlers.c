96663825f504e1f7856b481f4f681f1c51805783
aeis <[khh9201@ajou.ac.kr]>
2017-11-12 21:29:09 +0900

    signal handling

--- src/signal_handlers.c
+++ src/signal_handlers.c
@@ -1,11 +1,14 @@
 #include "signal_handlers.h"
+#include <stdio.h>
 
 void catch_sigint(int signalNo)
 {
   // TODO: File this!
+	printf("ctrl^c\n");
 }
 
 void catch_sigtstp(int signalNo)
 {
   // TODO: File this!
+	printf("ctrl^z\n");
 }
